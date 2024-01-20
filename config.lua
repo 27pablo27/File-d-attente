5a3d94560b9dd9d3b6cf532eb636b1361749c805
Pablo <148920646+27pablo27@users.noreply.github.com>
2024-01-20 23:37:14 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,92 @@
+Config = {}
+
+
+
+-- Temps d'attente Antispam
+Config.AntiSpamTimer = 2
+
+Config.TimerCheckPlaces = 3
+
+Config.TimerRefreshClient = 3
+
+Config.TimerUpdatePoints = 6
+
+
+Config.AddPoints = 1
+
+Config.RemovePoints = 1
+
+Config.LoterieBonusPoints = 25
+
+Config.Points = {
+	-- {'steamID', points},
+	-- {'steam:0123456789', 1000}
+}
+
+
+
+Config.NoSteam = "Steam n'a pas été détecté. Veuillez (re)lancer Steam et FiveM, puis réessayer."
+
+Config.EnRoute = "Vous êtes en route. Vous avez déjà parcouru"
+
+Config.PointsRP = "kilomètres"
+
+Config.Position = "Vous êtes en position "
+
+Config.EmojiMsg = "Si les emojis sont figés, relancez votre client : "
+
+Config.EmojiBoost = "!!! Youpi, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " gagnés !!!"
+
+Config.PleaseWait_1 = "Veuillez patienter "
+Config.PleaseWait_2 = " secondes. La connexion se lancera automatiquement !"
+
+Config.Accident = "Oups, vous venez d'avoir un accident... Si cela se reproduit, vous pouvez en informer le support :)"
+
+-- En cas de points négatifs / In case of negative points
+Config.Error = " ERREUR : RELANCEZ LA ROCADE ET CONTACTEZ LE SUPPORT DU SERVEUR "
+
+
+Config.EmojiList = {
+	'🐌', 
+	'🐍',
+	'🐎', 
+	'🐑', 
+	'🐒',
+	'🐘', 
+	'🐙', 
+	'🐛',
+	'🐜',
+	'🐝',
+	'🐞',
+	'🐟',
+	'🐠',
+	'🐡',
+	'🐢',
+	'🐤',
+	'🐦',
+	'🐧',
+	'🐩',
+	'🐫',
+	'🐬',
+	'🐲',
+	'🐳',
+	'🐴',
+	'🐅',
+	'🐈',
+	'🐉',
+	'🐋',
+	'🐀',
+	'🐇',
+	'🐏',
+	'🐐',
+	'🐓',
+	'🐕',
+	'🐖',
+	'🐪',
+	'🐆',
+	'🐄',
+	'🐃',
+	'🐂',
+	'🐁',
+	'🔥'
+}
